58c3159b48fc27a27b780e663c7d8887a13e0b66
Matheus Afonso de Souza <42722634+Matheusafonsouza@users.noreply.github.com>
2023-01-02 18:56:26 -0300

    Update longest with solution pattern for leetcode

--- longest-substring/main.py
+++ longest-substring/main.py
@@ -1,39 +1,27 @@
 from collections import Counter
 
 
-def divide_and_conquer(
-    s: str, 
-    k: int, 
-    start: int, 
-    end: int
-) -> int:
-    if start == end:
-        return 0
+class Solution:
+    def divide_and_conquer(self, s: str, k: int, start: int, end: int) -> int:
+        if start == end:
+            return 0
 
-    counter = Counter()
+        counter = Counter()
 
-    for i in range(start, end + 1):
-        counter[s[i]] += 1
+        for i in range(start, end + 1):
+            counter[s[i]] += 1
 
-    for i in range(start, end + 1):
-        current = s[i]
-        if counter[current] < k:
-            return max(divide_and_conquer(s, k, start , i - 1), divide_and_conquer(s, k, i + 1 , end))
+        for i in range(start, end + 1):
+            current = s[i]
+            if counter[current] < k:
+                return max(self.divide_and_conquer(s, k, start , i - 1), self.divide_and_conquer(s, k, i + 1 , end))
+            
+        return end - start + 1
+
+    def longestSubstring(self, s: str, k: int) -> int:
+        if k == 1:
+            return len(s)
+
+        return self.divide_and_conquer(s, k, 0, len(s) - 1)
+
         
-    return end - start + 1
-
-
-def longest_substring(
-    s: str, 
-    k: int
-) -> int:
-    if k == 1:
-        return len(s)
-
-    return divide_and_conquer(s, k, 0, len(s) - 1)
-
-    
-if __name__ == "__main__":
-    s = "aaabb"
-    k = 3
-    print(longest_substring(s, k))
